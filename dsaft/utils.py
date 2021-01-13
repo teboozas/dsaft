bbb134b326e5cbfd85ce7e6dcfb9837f871b5f6e
HYUNJUN LEE <lhj6363@gmail.com>
2021-01-13 22:34:38 +0900

    Update utils.py

--- dsaft/utils.py
+++ dsaft/utils.py
@@ -1,33 +1,44 @@
 import numpy as np
 import pandas as pd
 import warnings
-from lifelines import KaplanMeierFitter
+from lifelines import NelsonAalenFitter
 
-def get_surv(model, input):
+def get_surv(model, x_test, timegrid = "train"):
     '''
     model: PyCox model class or compatibles
-    input: covariate dataset to compute survival estimates
+    x_test: covariate dataset to compute survival estimates
     '''
     warnings.simplefilter(action='ignore', category=FutureWarning)
     
-    input, target = model.training_data
+    x_train, target = model.training_data
+    y_train, delta_train = target
+    
+    # compute residual from training data
+    exp_residual = np.exp(np.log(y_train) - model.predict(x_train).reshape(-1))
 
-    y, d = target
-    df = pd.DataFrame(y, d)
-    df.columns = ["y"]
-    df = df.sort_values(by = ["y"])
+    # compute exp(-theta) from test data to evaluate accelerating component
+    exp_predict = np.exp(-model.predict(x_test)).reshape(-1)
     
-    y_sorted = df.values.reshape(-1)
-    d_sorted = np.array(df.index, dtype = np.float32)
+    # estimate cumulative baseline hazard function
+    # based on training dataset
+    H = NelsonAalenFitter().fit(exp_residual, event_observed = delta_train).cumulative_hazard_
     
-    theta_test = model.predict(x_test)
-
-    e = (np.repeat(y_sorted.reshape(-1, 1), theta_test.shape[0], axis = 1) - np.exp(theta_test.reshape(-1)))
+    # extract timegrid and estimated hazards
+    time_grid = H.index.to_numpy()[1:]
+    H_base = H.values.reshape(-1)
     
-    surv_ = KaplanMeierFitter().fit(e.reshape(-1), np.array(list(d_sorted) * theta_test.shape[0])).survival_function_at_times(e.reshape(-1))
-    surv = pd.DataFrame(surv_.to_numpy().reshape(-1, theta_test.shape[0]))
-    surv.index = y_sorted
+    h_base = H_base[1:] - H_base[:-1]
+    h_base = np.repeat(h_base.reshape(-1, 1), exp_predict.shape[0], axis = 1)
+    
+    # evaluate conditional cumulative hazard estimates
+    # based on test dataset
+    surv = pd.DataFrame(np.exp(-np.cumsum(h_base * exp_predict, axis = 0)),
+                        index = time_grid, columns = [i for i in range(exp_predict.shape[0])])
     surv.index.names = ["duration"]
     
+    # set upperbound of time grid to "Y" of training dataset
+    # (to be comparable to survival predictions from PyCox models)
+    if timegrid == "train":
+        surv = surv.loc[surv.index <= y_train.max()]
+    
     return surv
-
