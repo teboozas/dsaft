49d0e45369444cdf399b308b7bbfea4e94e3924b
HYUNJUN LEE <lhj6363@gmail.com>
2021-01-14 22:27:49 +0900

    Update 'get_surv' function to deal with large data

--- dsaft/utils.py
+++ dsaft/utils.py
@@ -7,25 +7,39 @@ def get_surv(model, x_test, timegrid = "train"):
     '''
     model: PyCox model class or compatibles
     x_test: covariate dataset to compute survival estimates
+    timegrid: option to set upperbound of time grid to "Y" of training dataset
     '''
-    warnings.simplefilter(action='ignore', category=FutureWarning)
+    warnings.simplefilter(action='ignore', category = FutureWarning)
+    warnings.simplefilter(action='ignore', category = RuntimeWarning)
     
     x_train, target = model.training_data
     y_train, delta_train = target
     
     # compute residual from training data
-    exp_residual = np.exp(np.log(y_train) - model.predict(x_train).reshape(-1))
+    exp_residual = np.nan_to_num(np.exp(np.log(y_train) - model.predict(x_train).reshape(-1)))
 
     # compute exp(-theta) from test data to evaluate accelerating component
-    exp_predict = np.exp(-model.predict(x_test)).reshape(-1)
+    exp_predict = np.nan_to_num(np.exp(-model.predict(x_test)).reshape(-1))
     
     # estimate cumulative baseline hazard function
     # based on training dataset
     H = NelsonAalenFitter().fit(exp_residual, event_observed = delta_train).cumulative_hazard_
     
     # extract timegrid and estimated hazards
-    time_grid = H.index.to_numpy()[1:]
-    H_base = H.values.reshape(-1)
+    if timegrid == "train":
+        max_time = y_train.max()
+    else:
+        max_time = max(H.index)
+    
+    if H.shape[0] * exp_predict.shape[0] >= 5 * 10e7:        
+        l = round(5 * 10e7 / exp_predict.shape[0])
+        time_grid = np.quantile(a = H.loc[H.index <= max_time].index.values,
+                                q = [i / l for i in range(l + 1)],
+                                interpolation = 'nearest')
+    else:
+        time_grid = H.loc[H.index <= max_time].index.values
+    
+    H_base = H.loc[time_grid].values.reshape(-1)
     
     h_base = H_base[1:] - H_base[:-1]
     h_base = np.repeat(h_base.reshape(-1, 1), exp_predict.shape[0], axis = 1)
@@ -33,12 +47,9 @@ def get_surv(model, x_test, timegrid = "train"):
     # evaluate conditional cumulative hazard estimates
     # based on test dataset
     surv = pd.DataFrame(np.exp(-np.cumsum(h_base * exp_predict, axis = 0)),
-                        index = time_grid, columns = [i for i in range(exp_predict.shape[0])])
-    surv.index.names = ["duration"]
+                        index = time_grid[1:],
+                        columns = [i for i in range(exp_predict.shape[0])])
     
-    # set upperbound of time grid to "Y" of training dataset
-    # (to be comparable to survival predictions from PyCox models)
-    if timegrid == "train":
-        surv = surv.loc[surv.index <= y_train.max()]
+    surv.index.names = ["duration"]
     
     return surv
