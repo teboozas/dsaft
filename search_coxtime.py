4e61009f3d93aafa90ae3724482eaaa3d5b0fdda
leejunhyun <ljhyun33@korea.ac.kr>
2021-01-30 02:56:56 +0900

    Update search_coxtime.py

--- search_coxtime.py
+++ search_coxtime.py
@@ -28,56 +28,6 @@ from loss import DSAFTRankLoss,DSAFTMAELoss,DSAFTRMSELoss,DSAFTNKSPLLoss, DSAFTN
 from pycox.models import CoxTime
 from pycox.models.cox_time import MLPVanillaCoxTime, MixedInputMLPCoxTime
 
-def get_surv(model, x_test, timegrid = None):
-    '''
-    model: PyCox model class or compatibles
-    x_test: covariate dataset to compute survival estimates
-    timegrid: option to set upperbound of time grid to "Y" of training dataset
-    '''
-    warnings.simplefilter(action='ignore', category = FutureWarning)
-    warnings.simplefilter(action='ignore', category = RuntimeWarning)
-    
-    x_train, target = model.training_data
-    y_train, delta_train = target
-    
-    # compute residual from training data
-    # exp_residual = np.nan_to_num(np.exp(np.log(y_train) - model.predict(x_train).reshape(-1)))
-
-    # compute exp(-theta) from test data to evaluate accelerating component
-    exp_predict = np.nan_to_num(np.exp(-model.predict(x_test)).reshape(-1))
-    
-    # estimate cumulative baseline hazard function
-    # based on training dataset
-    H = NelsonAalenFitter().fit(y_train, event_observed = delta_train).cumulative_hazard_
-    
-    # extract timegrid and estimated hazards
-    if timegrid == "train":
-        max_time = y_train.max()
-    else:
-        max_time = max(H.index)
-    
-    if H.shape[0] * exp_predict.shape[0] >= 6 * 10e7:        
-        l = round(5 * 10e7 / exp_predict.shape[0])
-        time_grid = np.quantile(a = H.loc[H.index <= max_time].index.values,
-                                q = [i / l for i in range(l + 1)],
-                                interpolation = 'nearest')
-    else:
-        time_grid = H.loc[H.index <= max_time].index.values
-    
-    H_base = H.loc[time_grid].values.reshape(-1)
-    
-    h_base = H_base[1:] - H_base[:-1]
-    h_base = np.repeat(h_base.reshape(-1, 1), exp_predict.shape[0], axis = 1)
-    
-    # evaluate conditional cumulative hazard estimates
-    # based on test dataset
-    surv = pd.DataFrame(np.exp(-np.cumsum(h_base * exp_predict, axis = 0)),
-                        index = time_grid[1:],
-                        columns = [i for i in range(exp_predict.shape[0])])
-    
-    surv.index.names = ["duration"]
-    
-    return surv
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Survival analysis configuration')
@@ -92,7 +42,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--num_nodes', type=int, default=256)
     parser.add_argument('--num_layers', type=int, default=8)
-    parser.add_argument('--batch_size', type=int, default=1028)
+    parser.add_argument('--batch_size', type=int, default=1024)
     parser.add_argument('--epochs', type=int, default=512)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--weight_decay', type=float, default=0.0)
@@ -212,8 +162,8 @@ if __name__ == "__main__":
     lr = lrfinder.get_best_lr()
     model.optimizer.set_lr(lr)
 
-    wandb.init(project=args.dataset+'_coxtime', 
-            group=args.loss+'_'+args.optimizer,
+    wandb.init(project=args.dataset+'_baseline', 
+            group='coxtime'+'_'+args.optimizer,
             name=f'L{args.num_layers}N{args.num_nodes}D{args.dropout}W{args.weight_decay}B{args.batch_size}',
             config=args)
 
